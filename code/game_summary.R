a737f9b748f316ab05ff27fdd1b40bb943fdd028
cass-code <20346212@sun.ac.za>
2021-10-17 19:19:42 +0200

    Finalised all math equations

--- code/game_summary.R
+++ code/game_summary.R
@@ -3,7 +3,7 @@ game_summary <- function(){
         library(dplyr)
 
         Parameter <- c("$a$", "$c$", "$\\beta$", "$m$", "$\\delta$", "$d$")
-        Represents <- c("Potential assignment mark", "Cost of crisis to assignment mark", "Student's type: level of resiliency", "Mark penalty", "Lecturer's type: level of empathy", "Late deterrment utility")
+        Represents <- c("Potential assignment mark", "Cost of crisis to assignment mark", "Student's type: level of resiliency", "Mark penalty", "Lecturer's type: level of empathy", "Detterent")
         # Common <- c("Yes", "Yes", "No-only known by student", "Yes", "No-only known by lecturer", "Yes")
         Restriction <- c("$0\\leq a \\leq 1$", "$0 < c \\leq 1$", "$\\beta \\sim Uniform(0,1)$ ", "$0 < m \\leq 1$", "$\\delta \\sim Uniform(0,1)$", "$0<d \\leq 1$")
 
