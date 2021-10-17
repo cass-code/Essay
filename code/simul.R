120dd2856da4dbcfe031bccc26d222d2b1e34422
cass-code <20346212@sun.ac.za>
2021-10-17 16:55:57 +0200

    Updated model section and appendix
    Tried to reduce the explanation of the game in section: "A Model of Deadline Adherence "
    
    Moved some maths and tables into the appendix.
    
    Changed the word count on the front page (max 2000 limit).
    
    Working in the discussion section next.

--- code/simul.R
+++ code/simul.R
@@ -3,12 +3,11 @@ simul <- function(){
 library(dplyr)
 
 
-Penalty <- c("a-c, 0", "a-B(c) - m, D(m)")
-NP <- c("a-c, 0", "a-B(c), D(c)-d")
+Penalty <- c("$a-c, \\ 0$", "$a-\\beta c - m, \\ \\delta m$")
+NP <- c("$a-c, \\ 0$", "$a-\\beta c, \\ \\delta c -d$")
 
 frame <- data.frame(Penalty, NP) %>% rename("No Penalty" = NP)
 row.names(frame) <- c("On Time", "Late")
 frame
-
 
 }
