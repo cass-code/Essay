4ad289b3a82191365ebfcdda3c33ac250a1550f9
Cassandra Pengelly <80268603+cass-code@users.noreply.github.com>
2021-10-17 14:54:04 +0200

    Revert "Updated images/tables and model explanation"

--- code/game_summary.R
+++ code/game_summary.R
@@ -2,11 +2,13 @@ game_summary <- function(){
 
         library(dplyr)
 
-        Parameter <- c("$a$", "$c$", "$\\beta$", "$m$", "$\\delta$", "$d$")
+        Parameter <- c("a", "c", "B", "m", "D", "d")
         Represents <- c("Potential assignment mark", "Cost of crisis to assignment mark", "Student's type: level of resiliency", "Mark penalty", "Lecturer's type: level of empathy", "Late deterrment utility")
-        Restriction <- c("$0\\leq a \\leq 1$", "$0 < c \\leq 1$", "$\\beta \\sim Uniform$ ", "$0 < m \\leq 1$", "$\\delta \\sim Uniform$", "$0<d \\leq 1$")
+        Range <- c("0<=a=<1", "0<c=<1", "0<=B=<1", "0<m=<1", "0<=D=<1", "0=<d=<1")
 
-        frame <- data.frame(Parameter, Represents, Restriction) %>% rename("Parameter Represents" = Represents)
+        frame <- data.frame(Parameter, Represents, Range) %>% rename("Parameter Represents" = Represents)
         frame
+
+
 
 }
